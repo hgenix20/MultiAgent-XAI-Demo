548eb84ba5102d0a5713891c8ac9a07677d5bc24
hgenix20 <kameron.m.green@outlook.com>
2025-01-13 14:54:05 -0600

    Switched controller model

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig, pipeline
 
 try:
-    config = AutoConfig.from_pretrained("microsoft/Phi-3.5-MoE-instruct", trust_remote_code=True)
+    config = AutoConfig.from_pretrained("microsoft/Phi-3-mini-4k-instruct", trust_remote_code=True)
     print("Model configuration loaded successfully:")
     print(config)
 except KeyError as e:
@@ -39,10 +39,10 @@ Keep your responses concise. If the request is unethical or out of scope, polite
 
 @st.cache_resource
 def load_model_controller():
-    # Controller: microsoft/Phi-3.5-MoE-instruct
-    pipe = pipeline("text-generation", model="microsoft/Phi-3.5-MoE-instruct", trust_remote_code=True)
-    tokenizerC = AutoTokenizer.from_pretrained("microsoft/Phi-3.5-MoE-instruct", trust_remote_code=True)
-    modelC = AutoModelForCausalLM.from_pretrained("microsoft/Phi-3.5-MoE-instruct", trust_remote_code=True)
+    # Controller: microsoft/Phi-3-mini-4k-instruct
+    pipe = pipeline("text-generation", model="microsoft/Phi-3-mini-4k-instruct", trust_remote_code=True)
+    tokenizerC = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-4k-instruct", trust_remote_code=True)
+    modelC = AutoModelForCausalLM.from_pretrained("microsoft/Phi-3-mini-4k-instruct", trust_remote_code=True)
     return tokenizerC, modelC, pipe
 
 @st.cache_resource
