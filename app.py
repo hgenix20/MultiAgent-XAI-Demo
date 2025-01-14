0e7b360ced6fcf7bb8f470018df140006baab463
hgenix20 <kameron.m.green@outlook.com>
2025-01-13 23:37:59 -0600

    Abandoned DeepSeek and went back to phi-3-mini-4k-instruct

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4fde3b3e1f5ff50dc2f2af4315e4166c53ab61fe4bb8c7fce911fcae3d823eb
-size 6180
+oid sha256:2f8e96c5bf394608a675dcdea94360b43307651e5d3b337d35093316255a7430
+size 5032
